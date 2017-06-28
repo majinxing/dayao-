51f317ea1069096e370db69197af6a21a7693b27
1013258870@qq.com <1013258870@qq.com>
2017-06-28 18:54:43 +0800

    修改聊天页面的配置
    修改聊天页面

--- dayao/WisdomHall/MJXChatCellTableViewCell.m
+++ dayao/WisdomHall/MJXChatCellTableViewCell.m
@@ -10,6 +10,30 @@
 #import <Hyphenate/Hyphenate.h>
 
 #define RGBA_COLOR(R, G, B, A) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:A]
+
+@interface MJXChatCellTableViewCell()
+@property (weak, nonatomic) IBOutlet UITextView *firstTextView;
+@property (weak, nonatomic) IBOutlet UITextView *fifthTextView;
+@property (weak, nonatomic) IBOutlet UIImageView *secondImageVIew;
+@property (weak, nonatomic) IBOutlet UIImageView *sixthImageView;
+@property (weak, nonatomic) IBOutlet UIButton *thirdBtn;
+@property (weak, nonatomic) IBOutlet UIButton *seventhBtn;
+@property (weak, nonatomic) IBOutlet UILabel *fourthLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eigthLabel;
+@property (weak, nonatomic) IBOutlet UIButton *fourthBtn;
+@property (weak, nonatomic) IBOutlet UIButton *eigthBtn;
+@property (weak, nonatomic) IBOutlet UILabel *firstNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *secondNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *thirdNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fourthNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fifthNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sixthNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *seventhNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eigthNameLabel;
+
+
+@end
+
 @implementation MJXChatCellTableViewCell
 
 - (void)awakeFromNib {
@@ -23,7 +47,7 @@
     NSInteger index = 0;
     if (message.direction == EMMessageDirectionReceive) {
         switch (message.body.type) {
-            case EMMessageBodyTypeText://文字
+            case EMMessageBodyTypeText: //文字
                 identifier = @"MJXChatCellTableViewCellFirst";
                 index = 0;
                 break;
@@ -71,7 +95,34 @@
     if (!cell) {
         cell = [[[NSBundle mainBundle] loadNibNamed:@"MJXChatCellTableViewCell" owner:self options:nil] objectAtIndex:index];
     }
+    [cell setInfoWithNumber:index withMessage:message];
     return cell;
+}
+-(void)setInfoWithNumber:(NSInteger) n withMessage:(EMMessage *)message{
+    if (n==0) {
+        // 收到的文字消息
+        EMTextMessageBody *textBody = (EMTextMessageBody *)message.body;
+        _firstTextView.text = textBody.text;
+        _firstNameLabel.text = [NSString stringWithFormat:@"%@",message.from];
+    }else if (n==1){
+        
+    }else if (n==2){
+        
+    }else if (n==3){
+        
+    }else if (n==4){
+        EMTextMessageBody *textBody = (EMTextMessageBody *)message.body;
+        _fifthTextView.text = textBody.text;
+        _fifthNameLabel.text = message.from;
+    }else if (n==5){
+        
+    }else if (n==6){
+        
+    }else if (n==7){
+        
+    }else if (n==8){
+        
+    }
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
